2a3d277f8e6036ed712b41577a40324a4ec9a6eb
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-03-23 15:24:14 +0100

    adding theme change using contextType

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, Component } from 'react';
+
+export const ThemeContext = createContext();
+
+class ThemeContextProvider extends Component {
+    state = { 
+        isLightTheme: false,
+        light: { text: '#555', ui: '#ddd', bg: '#eee' },
+        dark: { text: '#ddd', ui: '#333', bg: '#555' }
+     } 
+    render() { 
+        return (
+            <ThemeContext.Provider value={{...this.state}}>
+                {this.props.children}
+            </ThemeContext.Provider>
+        );
+    }
+}
+ 
+export default ThemeContextProvider;
