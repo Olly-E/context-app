ef164cc292b92a052043db0ec4c17b053cc3f440
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-03-26 06:46:30 +0100

    converting a few state componnent and replacing them with useContext hooks

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React, {useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
-class BookList extends Component {
-    static contextType = ThemeContext;
-    render() { 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
+const BookList = () => {
+    const {isLightTheme, light, dark} = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return (
+    <div className='book-list' style={{color: theme.syntax, background: theme.bg}}>
                 <ul>
                 <li style={{background: theme.ui}}>the way of kings</li>
                 <li style={{background: theme.ui}}>the name of the wind</li>
@@ -15,7 +13,7 @@ class BookList extends Component {
                 </ul>
             </div>
         );
-    }
 }
+
+export default BookList
  
-export default BookList;
--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,14 +1,16 @@
-import React, {Component} from 'react';
+import React, { useContext} from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
-class ThemeToggle extends Component {
-    static contextType = ThemeContext;
-    render() { 
+
+const ThemeToggle = () => {
+    const { toggleTheme } = useContext(ThemeContext);
+  return (
+    <div>
         const { toggleTheme } = this.context;
         return (
             <button onClick={toggleTheme}>Toggle the theme</button>
-        );
-    }
+    </div>
+  )
 }
- 
-export default ThemeToggle;
+
+export default ThemeToggle
